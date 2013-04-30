ddd00f2451a41d5501314440db5b3ae06d6f5990
Jeffrey Van Alstine <jvanalstine1@gmail.com>
2013-04-30 14:47:26 -0600

    Initial commit

---
+++ app/scripts/app.js
@@ -0,0 +1,13 @@
+'use strict';
+
+angular.module('jqueryMobileAngularAdapterBugTestApp', [])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
